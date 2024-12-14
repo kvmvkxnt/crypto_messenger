d9bb74d91fef56adf3e548edd44243cb56023d54
kvmvkxnt <kamron1007dada@gmail.com>
2024-12-14 19:04:22 +0300

    huy

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from network.sync import SyncManager
 from utils import config as cfg
 from utils import logger
 import socket
+import time
 
 log = logger.Logger("main")
 blockchain = Blockchain(cfg.BLOCK_DIFFICULTY)
@@ -41,43 +42,47 @@ broadcast_port = input(f"Enter your broadcast \
 port(default={cfg.BROADCAST_PORT}): ") or cfg.BROADCAST_PORT
 log.debug(f"Selected broadcast port: {broadcast_port}")
 
-network = P2PNetwork(P2PSocket(host, int(port)), int(broadcast_port))
-network.start()
-network.discover_peers(discover_peers)
+#network = P2PNetwork(P2PSocket(host, int(port)), int(broadcast_port))
+#network.start()
+#network.discover_peers(discover_peers)
 
-
-print("""
-    If you're using any vpn or proxy, please turn it off
-    1. Connect to peer
-    2. Broadcast message (public, manual)
-    3. Sync with other peers (manual)
-    4. Discover other peers (manual)
-    5. List peers
-    6. Exit
-""")
-
+peers = discover_peers(host, int(port), int(broadcast_port))
 while True:
-    try:
-        user_input = int(input("Choice: "))
-    except ValueError:
-        print("Not an option. Please, choose another")
-        continue
+    print(list(peers))
+    time.sleep(5)
 
-    if user_input > 6 or user_input < 1:
-        print("Not an option. Please, choose another")
-    elif user_input == 6:
-        break
-    elif user_input == 5:
-        print(network.peers)
-        if len(network.peers):
-            for i in network.peers:
-                print(i)
-        else:
-            print("No peers")
-    elif user_input == 4:
-        message = input("Enter a message: ")
-        network.broadcast_message(message)
-    elif user_input == 1:
-        peer_host = input("Enter peer's ip: ")
-        peer_port = int(input("Enter peer's port: "))
-        network.connect_to_peer(peer_host, peer_port)
+#print("""
+#    If you're using any vpn or proxy, please turn it off
+#    1. Connect to peer
+#    2. Broadcast message (public, manual)
+#    3. Sync with other peers (manual)
+#    4. Discover other peers (manual)
+#    5. List peers
+#    6. Exit
+#""")
+#
+#while True:
+#    try:
+#        user_input = int(input("Choice: "))
+#    except ValueError:
+#        print("Not an option. Please, choose another")
+#        continue
+#
+#    if user_input > 6 or user_input < 1:
+#        print("Not an option. Please, choose another")
+#    elif user_input == 6:
+#        break
+#    elif user_input == 5:
+#        print(network.peers)
+#        if len(network.peers):
+#            for i in network.peers:
+#                print(i)
+#        else:
+#            print("No peers")
+#    elif user_input == 4:
+#        message = input("Enter a message: ")
+#        network.broadcast_message(message)
+#    elif user_input == 1:
+#        peer_host = input("Enter peer's ip: ")
+#        peer_port = int(input("Enter peer's port: "))
+#        network.connect_to_peer(peer_host, peer_port)
