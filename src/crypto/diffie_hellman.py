da03644360a38dd91b5d988ee4fc9b1e6ed6174f
kvmvkxnt <kamron1007dada@gmail.com>
2024-12-15 14:00:56 +0300

    yo mama

--- src/crypto/diffie_hellman.py
+++ src/crypto/diffie_hellman.py
@@ -35,9 +35,10 @@ class DiffieHellmanKeyExchange:
             :type parameters: DHParameters or None
         '''
         # Getting parameters for DH or generating them
-        self.parameters = parameters or \
-            dh.generate_parameters(generator=2, key_size=2048,
-                                   backend=default_backend())
+        self.parameters = parameters or dh.DHParameterNumbers(
+            0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF,
+            2
+        ).parameters(default_backend())
         self.private_key = self.parameters.generate_private_key()
         self.public_key = self.private_key.public_key()
 
