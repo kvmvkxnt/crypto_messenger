14dce0ae70d90ff6b1b56a7ed0747f4b6ed5bea4
kvmvkxnt <kamron1007dada@gmail.com>
2024-12-16 18:45:49 +0300

    Fixed bugs in discovery

--- src/network/discovery.py
+++ src/network/discovery.py
@@ -3,6 +3,8 @@ import threading
 import time
 import utils.logger as logger
 from typing import Set, Tuple
+import json
+import os
 
 log = logger.Logger("discovery")
 
@@ -11,9 +13,9 @@ def discover_peers(
     local_host: str,
     local_port: int,
     broadcast_port: int,
+    username: str,
+    public_key: str,
     broadcast_interval: int = 1,
-    timeout: int = 5,
-    peers: Set[Tuple[str, int]] = set(),
 ) -> Set[Tuple[str, int]]:
     """
     Обнаружение новых узлов в сети через UDP широковещательные сообщения.
@@ -24,40 +26,41 @@ def discover_peers(
     :param broadcast_interval: Интервал отправки широковещательных сообщений
     :param timeout: таймаут
     """
-    stop_event = threading.Event()
-
+    peers = set()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     def listen_for_broadcast():
         """Слушает широковещательные сообщения для обнаружения новых узлов."""
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+        with sock as udp_socket:
             udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
             try:
-                udp_socket.bind((local_host, broadcast_port))
+                if os.name == "posix":
+                    udp_socket.bind(('', broadcast_port))
+                else:
+                    udp_socket.bind((local_host, broadcast_port))
             except socket.error as e:
                 log.error(f"Could not bind socket: {e}")
                 return
 
             log.debug(f"Listening for broadcasts on {local_host}:{broadcast_port}")
-            start_time = time.time()
-            while not stop_event.is_set() and time.time() - start_time < timeout:
+            while True:
                 try:
                     data, addr = udp_socket.recvfrom(1024)
-                    peer_info = data.decode()
+                    peer_info = json.loads(data.decode())
                     if ":" not in peer_info:
                         log.debug(f"Invalid peer info received: {peer_info}")
                         continue
 
-                    peer_host, peer_port = peer_info.split(":")
+                    peer_port = peer_info["port"]
                     try:
                         peer_port = int(peer_port)
                     except ValueError:
                         log.debug(f"Invalid peer port received: {peer_port}")
                         continue
 
-                    peer = (addr[0], peer_port)
-                    if peer == (local_host, local_port) or (peer_host, peer_port) == (
-                        local_host,
-                        local_port,
-                    ):
+                    peer = (addr[0], peer_port, peer_info["username"],
+                            peer_info["public_key"])
+                    if peer == (local_host, local_port, username, public_key):
                         log.debug(f"Ignoring self broadcast from {peer}")
                         continue
 
@@ -74,15 +77,17 @@ def discover_peers(
         Отправляет широковещательное сообщение
         для оповещения о своем узле.
         """
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+        with sock as udp_socket:
             udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-            message = f"{local_host}:{local_port}"
+            message = {"host": local_host, "port": local_port,
+                       "public_key": public_key, "username": username}
             broadcast_address = ("<broadcast>", broadcast_port)
 
-            while not stop_event.is_set():
+            while True:
                 try:
-                    udp_socket.sendto(message.encode(), broadcast_address)
+                    udp_socket.sendto(json.dumps(message, ensure_ascii=False)
+                                      .encode(), broadcast_address)
                     log.debug(f"Broadcasting: {message}")
                 except socket.error as e:
                     log.error(f"Error in sending broadcast: {e}")
@@ -94,8 +99,7 @@ def discover_peers(
     threading.Thread(target=listen_for_broadcast, daemon=True).start()
     threading.Thread(target=send_broadcast, daemon=True).start()
 
-    time.sleep(timeout)
-    stop_event.set()
+    return peers
 
 
 if __name__ == "__main__":
